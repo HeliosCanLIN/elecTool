78fb93501b53f05becd25e1e7b43cf7aed9d6fee
Caan Lam <469187333@qq.com>
2020-08-21 17:43:48 +0800

    v0.8.2-beta

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from selenium import webdriver
-from selenium.webdriver import ActionChains
-import pyperclip
-import win32api
-import win32con
-from openpyxl import load_workbook
-from openpyxl.utils import get_column_letter, column_index_from_string
+from selenium import webdriver #实例化浏览器
+from selenium.webdriver import ActionChains #浏览器自动操作
+import pyperclip #获取剪切板
+import win32api #非input标签上传文件需要的系统库
+import win32con #非input标签上传文件需要的系统库
+from openpyxl import load_workbook #读取Excel文件
+from openpyxl.utils import get_column_letter, column_index_from_string #操作Excel文件
 import datetime
 import time
-import json
+import json #读取json文件
 import sys
-import yaml
-import requests
-import re
-from bs4 import BeautifulSoup
-import queue
+import yaml #读取yaml配置文件
+import requests #发送报文请求
+import re #正则表达式
+from bs4 import BeautifulSoup #处理html
+import queue #队列
 
-version = "v0.8.0-beta"
-versionNum = "0814.1"
+version = "v0.8.2-beta"
+versionNum = "0821.1"
 
 def get_yaml_data(yaml_file): #读取配置文件
     # 打开yaml文件
@@ -83,8 +83,9 @@ def UpLoad_File(webEle, filePath): #非ipunt标签的上传附件功能
 
 def checkData(): #检查当前录入缴费的所需要的资料
     global period
-    e_xlsx = load_workbook(path+"非平峰谷电费缴费导入模板(软件用).xlsx")
-    s1 = e_xlsx['非平峰谷']
+    #e_xlsx = load_workbook(path+"非平峰谷电费缴费导入模板(软件用).xlsx")
+    #s1 = e_xlsx['非平峰谷']
+    s1 = template['非平峰谷']
     meter = s1['F2']
     percent = s1['H2']
     dateStr = s1['C2']
@@ -126,11 +127,19 @@ def checkData(): #检查当前录入缴费的所需要的资料
     i = 0
     for cell in col:
         i += 1
+        """
         if cell.value==meter.value and str(sht['E'+cell.coordinate[1:]].value)==str(period):
             regionLocat = 'A' + cell.coordinate[1:]
             billNo = 'D' + cell.coordinate[1:]
             billDate = 'E' + cell.coordinate[1:]
             break
+        """
+        if cell.value==meter.value :
+            regionLocat = 'A' + cell.coordinate[1:]
+            billNo = 'D' + cell.coordinate[1:]
+            billDate = 'E' + cell.coordinate[1:]
+            break
+
     region = sht[regionLocat] #获取报账点区域
     Date = sht[billDate]
     No = sht[billNo]
@@ -148,11 +157,14 @@ def checkData(): #检查当前录入缴费的所需要的资料
         if share=="共享":
             filePath = "\"E:\Work\\0759\\QT-001-直供电" + share + "清单（电子版）.xlsx\" \"E:\Work\\0759\\QT-002-" + periodYear + "年" + periodMonth + "月供电局清单（电子版）.xlsx\" \"" + letterPath + "\""
             billPath = "\"E:\Work\\0759\\影像资料PDF\\"+periodYear+"-"+periodMonth+"影像资料\\直供电\\"+No.value+"\""
+        if share=="独享":
+            filePath = "\"E:\Work\\0759\\QT-001-直供电" + share + "清单（电子版）.xlsx\" \"E:\Work\\0759\\QT-002-" + periodYear + "年" + periodMonth + "月供电局清单（电子版）.xlsx\""
+            billPath = "\"E:\Work\\0759\\影像资料PDF\\"+periodYear+"-"+periodMonth+"影像资料\\直供电\\"+No.value+"\""
     print(filePath)
     return filePath
     xlsx2.close()
 
-def login(user): #自动输入账号密码
+def login(user): #登录
     element = browser.find_element_by_id("area_name")
     ActionChains(browser).move_to_element(element).perform()
     element.click()
@@ -193,7 +205,7 @@ def Import(): #批量导入功能的的主函数
         j = 0
         for cell in row: #遍历行
             j += 1
-            if j==column_index_from_string('AD') or j==column_index_from_string('AE')or j==column_index_from_string('AY')or j==column_index_from_string('BA')or j==column_index_from_string('BB'):
+            if j==column_index_from_string('AD') or j==column_index_from_string('AE')or j==column_index_from_string('AY')or j==column_index_from_string('BA')or j==column_index_from_string('BB')or j==column_index_from_string('BH')or j==column_index_from_string('BJ')or j==column_index_from_string('BK'):
                 if cell.value == None:
                     data =" "
                 else:
@@ -241,10 +253,11 @@ def elecAccountImport(index): #导入电费时的网页操作函数
 
     f1 = browser.find_element_by_xpath("/html/body/div[4]/div/div/div[2]/div/ul[1]/li/div[2]/a")
     UpLoad_File(f1,file) #非input标签上传文件 #上传附件
-    time.sleep(1)
+    time.sleep(0.5)
     element = browser.find_element_by_xpath("/html/body/div[7]/div/div/div[3]/a")
     ActionChains(browser).move_to_element(element).perform()
     element.click()
+    time.sleep(0.5)
     if int(period) <= 201901:
         f1 = browser.find_element_by_xpath("/html/body/div[4]/div/div/div[2]/div/ul[1]/li/div[2]/a")
         UpLoad_File(f1, billPath)  # 非input标签上传文件 #上传附件
@@ -252,11 +265,11 @@ def elecAccountImport(index): #导入电费时的网页操作函数
         element = browser.find_element_by_xpath("/html/body/div[7]/div/div/div[3]/a")
         ActionChains(browser).move_to_element(element).perform()
         element.click()
-    time.sleep(1)
+        time.sleep(1)
     element = browser.find_element_by_xpath("/html/body/div[4]/div/div/div[3]/a[2]")
     ActionChains(browser).move_to_element(element).perform()
     element.click()
-    time.sleep(0.5)
+    time.sleep(0.2)
 
     while 1:
         try:
@@ -284,6 +297,132 @@ def readConfig():
     userName2 = config['userName2']
     user2 = config['user2']
     password2 = config['password2']
+
+def benchmark():
+    xlsx1 = load_workbook(path + "标杆数据.xlsx")
+    s1 = xlsx1['Sheet1']
+    col1 = s1['A']
+    q = queue.Queue()
+    url1 = "http://10.217.240.219:8090/NCMS/asserts/tpl/selfelec/payment/showBenchmark"  # 接口地址
+    # js = "window.open('http://10.217.240.219:8090/NCMS/asserts/tpl/selfelec/payment_finance/manage.html?formData=%7B%22billamountDateOpen%22:%222020-08-13%22%7D')"
+    js = "window.open('http://10.217.240.219:8090/NCMS/asserts/tpl/selfelec/payment_finance/manage.html')"
+    browser.execute_script(js)
+    time.sleep(0.5)
+    page = browser.window_handles
+    browser.switch_to_window(page[1])  # 切换至明细页面
+    time.sleep(1)
+    flag = input("请调整好爬取的页面,按1继续\n>>>");
+    if flag == "1":
+        i = 1
+        for link in browser.find_elements_by_xpath("//tbody//*[@href]"):  # 此循环请求速度极快 有被服务器ban掉的可能
+            accountLink = link.get_attribute('href')
+            accountId = re.findall(r"billaccountpaymentdetailId=(.+?)&", accountLink, flags=0)
+            # 消息头数据
+            headers = {
+                'Connection': 'keep-alive',
+                'Content-Length': '59',
+                'Accept': '*/*',
+                'DNT': '1',
+                'X-Requested-With': 'XMLHttpRequest',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Origin': 'http://10.217.240.219:8090',
+                'Referer': accountLink,
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Cookie': '/NCMS/welcomecuNum=1; SESSION=' + cookie['value'],
+
+            }
+            # 消息数据
+            data = {'billaccountpaymentdetailId': accountId[0]}
+            r = requests.post(url1, headers=headers, data=data, verify=False)  # 发送POST请求标杆
+            packet = r.json()  # 获取回复
+            html = packet['obj']  # 提取html
+            """
+            try:
+                soup = BeautifulSoup(html, 'html.parser')  # 将html实例化为BeautifulSoup对象
+            except:
+                print("html实例化失败,检查你的cookie是否正常获取")
+            """
+            soup = BeautifulSoup(html, 'html.parser')  # 将html实例化为BeautifulSoup对象
+            for item in soup.find_all("td"):  # 遍历html提取table
+                benchmark = re.sub('\s', ' ', item.get_text())  # 去除html标签
+                print(benchmark)
+                q.put(benchmark)  # 数值推入队列
+            account = browser.find_element_by_xpath(
+                "/html/body/div[4]/div[2]/div[2]/table/tbody/tr[" + str(i) + "]/td[7]").text  # 获取缴费单号
+            accountName = browser.find_element_by_xpath(
+                "/html/body/div[4]/div[2]/div[2]/table/tbody/tr[" + str(i) + "]/td[8]").text  # 获取缴费单号
+            accountDate = browser.find_element_by_xpath(
+                "/html/body/div[4]/div[2]/div[2]/table/tbody/tr[" + str(i) + "]/td[11]").text  # 获取缴费单号
+            DateStr = browser.find_element_by_xpath(
+                "/html/body/div[4]/div[2]/div[2]/table/tbody/tr[" + str(i) + "]/td[12]").text  # 获取缴费单号
+            DateEnd = browser.find_element_by_xpath(
+                "/html/body/div[4]/div[2]/div[2]/table/tbody/tr[" + str(i) + "]/td[13]").text  # 获取缴费单号
+            print(account)
+            k = 1
+            for k in range((i * 4 - 3), (i * 4 + 1)):  # 遍历列
+                k += 1
+                s1['E' + str(k)].value = str(account)
+                s1['F' + str(k)].value = str(accountName)
+                s1['G' + str(k)].value = str(accountDate)
+                s1['H' + str(k)].value = str(DateStr)
+                s1['I' + str(k)].value = str(DateEnd)
+                row = s1[str(k)]
+                j = 0
+                for cell in row:  # 遍历行
+                    j += 1
+                    if j == 5:
+                        break
+                    cell.value = q.get()
+            i += 1
+        xlsx1.save(path + "标杆数据.xlsx")
+
+def histoicFlowTime():
+    url1 = "http://10.217.240.219:8090/NCMS/act/common/histoicFlowTime"  # 接口地址
+    js = "window.open('http://10.217.240.219:8090/NCMS/asserts/tpl/selfelec/payment_finance/manage.html')"
+    browser.execute_script(js)
+    time.sleep(0.5)
+    page = browser.window_handles
+    browser.switch_to_window(page[1])  # 切换至明细页面
+    time.sleep(1)
+    user = input("请调整好爬取的页面,按1继续\n>>>");
+    if user == "1":
+        i = 1
+        for link in browser.find_elements_by_xpath("//tbody//*[@href]"):  # 此循环请求速度极快 有被服务器ban掉的可能
+            accountLink = link.get_attribute('href')
+            accountId = re.findall(r"billaccountpaymentdetailId=(.+?)&", accountLink, flags=0)
+            # 消息头数据
+            headers = {
+                'Connection': 'keep-alive',
+                'Content-Length': '57',
+                'Accept': 'application/json, text/javascript, */*; q=0.01',
+                'DNT': '1',
+                'X-Requested-With': 'XMLHttpRequest',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Origin': 'http://10.217.240.219:8090',
+                'Referer': accountLink,
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Cookie': '/NCMS/welcomecuNum=1; SESSION=' + cookie['value'],
+
+            }
+            # 消息数据
+            data = {
+                'tableName': 'ele_payment',
+                'id': accountId[0],
+            }
+
+            r = requests.post(url1, headers=headers, data=data, verify=False)  # 发送POST请求标杆
+            packet = r.json()  # 获取回复
+            obj = str(packet['obj'])
+            str_info=obj.strip( '[]' )
+            #str_info = test1.replace("\'", "\"")
+            dict_info = eval(str_info)
+            dataMax=len(dict_info)
+            print(eval(str(dict_info[dataMax-2]))['comment'])
+
 
 if __name__ == '__main__':
     try:
@@ -316,11 +455,12 @@ if __name__ == '__main__':
         browser.get(url)
     # 再次访问页面，便可实现免登陆访问
     while 1:
-        print("(1) - 直供电电费导入(在出账工具出账后使用,使用时需要关闭出账工具和模板)")
-        print("(2) - 登录,获取cookie(登录后使用,获取后当天有效无需再登录)")
+        print("(1) - 直供电电费导入(在出账工具出账后使用)")
+        print("(2) - 登录,获取cookie")
         print("(3) - 报账点批量启用-(不可靠)")
-        print("(4) - 超标备注(alpha)")
+        print("(4) - 超标备注(不可靠)")
         print("(5) - 爬取标杆(beta)")
+        print("(6) - 爬取审核信息(beta)")
         print("(0) - 退出")
         userInput = input("输入数字选择功能\n>>>");
         if userInput == "0":
@@ -331,7 +471,7 @@ if __name__ == '__main__':
             page = browser.window_handles
             browser.switch_to_window(page[1])  # 切换至缴费页面
             try:
-                tool = load_workbook(path+"三家运营商代垫电费出账工具V35V1-20200803（更新电表在用运营商）.xlsm")
+                tool = load_workbook(path+"三家运营商代垫电费出账工具V35V1-20200818.xlsm")
                 template = load_workbook(path+"非平峰谷电费缴费导入模板(软件用).xlsx")
                 toolSheet = tool['新版导入表']
                 templateSheet = template['非平峰谷']
@@ -430,72 +570,10 @@ if __name__ == '__main__':
                 j += 1
                 if str(cell.value) == test:
                     print(s2['C' + cell.coordinate[1:]].value)
-
         if userInput == "5":
-            xlsx1 = load_workbook(path + "标杆数据.xlsx")
-            s1 = xlsx1['Sheet1']
-            col1=s1['A']
-            q = queue.Queue()
-            url1 = "http://10.217.240.219:8090/NCMS/asserts/tpl/selfelec/payment/showBenchmark" # 接口地址
-            #js = "window.open('http://10.217.240.219:8090/NCMS/asserts/tpl/selfelec/payment_finance/manage.html?formData=%7B%22billamountDateOpen%22:%222020-08-13%22%7D')"
-            js = "window.open('http://10.217.240.219:8090/NCMS/asserts/tpl/selfelec/payment_finance/manage.html')"
-            browser.execute_script(js)
-            time.sleep(0.5)
-            page = browser.window_handles
-            browser.switch_to_window(page[1])  # 切换至缴费页面
-            time.sleep(1)
-            user = input("请调整好爬取的页面,按1继续\n>>>");
-            if user=="1":
-                i = 1
-                for link in browser.find_elements_by_xpath("//tbody//*[@href]"):  # 此循环请求速度极快 有被服务器ban掉的可能
-                    # print(link.get_attribute('href'))
-                    accountLink = link.get_attribute('href')
-                    accountId = re.findall(r"billaccountpaymentdetailId=(.+?)&", accountLink, flags=0)
-                    # print(str(accountId[0]))
-                    # 消息头数据
-                    headers = {
-                        'Connection': 'keep-alive',
-                        'Content-Length': '59',
-                        'Accept': '*/*',
-                        'DNT': '1',
-                        'X-Requested-With': 'XMLHttpRequest',
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
-                        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                        'Origin': 'http://10.217.240.219:8090',
-                        'Referer': accountLink,
-                        'Accept-Encoding': 'gzip, deflate',
-                        'Accept-Language': 'zh-CN,zh;q=0.9',
-                        'Cookie': 'SESSION=4f24766f-7be6-48ee-9dc5-04318fd6b9c1; /NCMS/welcomecuNum=1; prvCode=GD; ifShow=false; UM_distinctid=173e5f3600a58b-075ec3bf36861e-3323767-1fa400-173e5f3600b7f3; CNZZDATA155540=cnzz_eid%3D1059811877-1597288610-%26ntime%3D1597288610',
-
-                    }
-                    # 消息数据
-                    data = {'billaccountpaymentdetailId': accountId[0]}
-
-                    r = requests.post(url1, headers=headers, data=data, verify=False)  # 发送POST请求标杆
-                    packet = r.json()  # 获取回复
-                    html = packet['obj']  # 提取html
-
-                    soup = BeautifulSoup(html, 'html.parser')  # 将html实例化为BeautifulSoup对象
-                    for item in soup.find_all("td"):  # 遍历html提取table
-                        benchmark = re.sub('\s', ' ', item.get_text())  # 去除html标签
-                        print(benchmark)
-                        q.put(benchmark)  # 数值推入队列
-                    account = browser.find_element_by_xpath(
-                        "/html/body/div[4]/div[2]/div[2]/table/tbody/tr[" + str(i) + "]/td[7]").text  # 获取缴费单号
-                    print(account)
-                    k = 1
-                    for k in range((i * 5 - 4), (i * 5 + 1)):  # 遍历列
-                        k += 1
-                        s1['E' + str(k)].value = str(account)
-                        row = s1[str(k)]
-                        j = 0
-                        for cell in row:  # 遍历行
-                            j += 1
-                            if j == 5:
-                                break
-                            cell.value = q.get()
-                    i += 1
-                xlsx1.save(path + "标杆数据.xlsx")
+            benchmark()
+        if userInput == '6':
+            histoicFlowTime()
         if userInput == "V" or userInput == "v":
             print(version)
     browser.switch_to_window(page[0])
